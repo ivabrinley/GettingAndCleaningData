c8f9ea8164a5875f691b240e8553230ac1076588
ivabrinley <76880701+ivabrinley@users.noreply.github.com>
2021-02-15 21:40:35 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,72 +1,58 @@
-##Find out what the working directory is
-getwd()
-
 ##First, we will merge the training and test data sets to create one combined data set
 
 ##Set up the working directory to be the folder where the files are located
 setwd("C:/Users/ivabr/Documents/UCI HAR Datasets")
-list.files(getwd(),pattern=".*.txt")
+list.files(getwd(),pattern=".*.txt") ##Checking to see what files are located in the path
 
-##Create data frames Activities and Features for the respective files containing these elements
 
+##Create data frames "Activities" and "Features" for the respective files containing these elements in the path
 Activities <- read.table("activity_labels.txt", header = FALSE)
 Features <- read.table ("features.txt", header = FALSE)
 
+
 ##Set new working directory to be the folder where the test data is located
-##Check to see what the files are there and subsequently create vectors for each of
-##the datasets
-
 setwd("C:/Users/ivabr/Documents/UCI HAR Datasets/test")
-list.files(getwd(),pattern=".*.txt")
+list.files(getwd(),pattern=".*.txt") ##Checking to see what files are located in the path
 
-subject_test <- read.table("subject_test.txt", header = FALSE)
-X_test <- read.table("X_test.txt", header = FALSE)
-y_test <- read.table("y_test.txt", header = FALSE)
+##Create data frames for each of the respective files that we will need the data elements from
+subject_test <- read.table("subject_test.txt", header = FALSE) ##Data frame for subject_test data
+X_test <- read.table("X_test.txt", header = FALSE) ##Data frame for X_test data
+y_test <- read.table("y_test.txt", header = FALSE) ##Data frame for y_test data
 
 ##Set your new working directory to be the folder where the train data is located
-##Check to see what the files are there and subsequently create vectors for each of
-##the datasets
-
 setwd("C:/Users/ivabr/Documents/UCI HAR Datasets/train")
-list.files(getwd(),pattern=".*.txt")
+list.files(getwd(),pattern=".*.txt") ##Checking to see what files are located in the path
 
-subject_train <- read.table("subject_train.txt", header = FALSE)
-X_train <- read.table("X_train.txt", header = FALSE)
-y_train <- read.table("y_train.txt", header = FALSE)
+##Create data frames for each of the respective files that we will need the data elements from
+subject_train <- read.table("subject_train.txt", header = FALSE) ##Data frame for subject_train data
+X_train <- read.table("X_train.txt", header = FALSE) ##Data frame for X_train data
+y_train <- read.table("y_train.txt", header = FALSE) ##Data frame for y_train data
 
 
 ##1. Merge training and test datasets using rbind
+SubjectMerge <- rbind(subject_test, subject_train) ##merging the subject_test and subject_train datasets
+YMerge<- rbind(y_test, y_train) ##Merging the y_test and y_train datasets
+XMerge<- rbind(X_test, X_train) ##Merging the x_test and x_train datasets
 
-SubjectMerge <- rbind(subject_test, subject_train)
-YMerge<- rbind(y_train, y_test)
-XMerge<- rbind(X_train, X_test)
+##Set names for the elements in each merged dataset
+names(SubjectMerge)<-c("subject") ##setting name "subject" for the field of the merged subject train and test merged datasets
+names(YMerge)<- c("activity") ##assigning name "activity" for column in y_test and y_train merged datasets
+names(XMerge)<- Features$V2 ##using second column of data frame for Features to assign names for the x_test and x_train merged datasets
 
-##set names to variables
-names(SubjectMerge)<-c("subject")
-names(YMerge)<- c("activity")
-Xnames <- Features
-names(XMerge)<- Xnames$V2
 
-##Merge columns and create ALLData combining all the data
-CombineAll <- cbind(SubjectMerge, YMerge)
-ALLData <- cbind(XMerge, CombineAll)
+##Final merge of all columns and datasets
+CombineAll <- cbind(SubjectMerge, YMerge) ##combine "subject" merged datasets with activities datasets
+ALLData <- cbind(XMerge, CombineAll) ##combine features and activities 
 
-##2.Now we are going to extract only the mean and standard deviation for each measurement
-##using sapply as a function
 
-SubjectMean<-sapply(SubjectMerge, mean, na.rm=TRUE)  
-SubjectSD<-sapply(SubjectMerge, sd, na.rm=TRUE) 
+##2.Now we are going to extract only the mean and standard deviation for each measurement using sapply as a function
 
-XMean<-sapply(XMerge, mean, na.rm=TRUE)  
-XStDev<-sapply(XMerge, sd, na.rm=TRUE)  
-
-YMean<-sapply(YMerge, mean, na.rm=TRUE)  
-YSDDev<-sapply(YMerge, sd, na.rm=TRUE) 
+XMean<-sapply(XMerge, mean, na.rm=TRUE) ##Mean for the features 
+XStDev<-sapply(XMerge, sd, na.rm=TRUE)  ##St. Dev for the features
 
 
 
-##3.Next, we are going to use descriptive activity names to name the activities
-##in the dataset
+##3.Next, we are going to use descriptive activity names to name the activities in the dataset
 
 head(Activities)
 
@@ -79,30 +65,20 @@ ALLData$activity[ALLData$activity==6] <- "LAYING"
 
 ##4. Next, we need to appropriately label the dataset with descriptive variable names
 
-##let's read the feature names from the features file
-featurenames   <- read.table("./features.txt")
-featurenames
-
 ##let's see the current names of the new dataset and then use gsub to fix
 
 names(ALLData)
 
-FixTime<-gsub("^t", "time", names(ALLData))
-FixFrequency<-gsub("^f", "frequency", names(ALLData))
-cleaner <- function(featurenames) {
-  tolower(gsub("(\\(|\\)|\\-)","",featurenames))
-}
+##Remove all the brackets and "-" characters to clean the names of the columns with gsub function
+names(ALLData) <- gsub("(\\(|\\)|\\-)","", names(ALLData)) 
 
 
 ##5. From the new dataset, we will now create a second, 
-#independent tidy data set with the average of each variable for each activity and each subject.
+##independent tidy data set with the average of each variable for each activity and each subject.
 
 library(dplyr)
 
-NEWdata<-aggregate(. ~subject + activity, ALLData, mean)
-
-NEWdata<-NEWdata[order(NEWdata$subject,NEWdata$activity),]
+##taking the average for each activity and each subject
+NEWdata<-aggregate(. ~subject + activity, ALLData, mean) 
 
 write.table(NEWdata, file = "NEWdata.txt",row.name=FALSE)
-
-NEWdata
